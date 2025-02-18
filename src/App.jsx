8c31b136e6d986d186f9fe25c50740b2695dbae3
ssandyy <ssandyy789@gmail.com>
2025-02-18 05:43:15 +0530

    props and objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import About from './screens/About'
+import Contact from './screens/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +9,8 @@ function App() {
   return (
     <>
       <h1>Hello React..!</h1>
+      <About />
+      <Contact />
     </>
   )
 }
