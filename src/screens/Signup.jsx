17524b61d3b19dbf961396833bf5e1670dda376c
ssandyy <ssandyy789@gmail.com>
2025-02-20 07:18:46 +0530

    useEffect(),

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../style/signup.css";
 
 // Use of Hooks (useEffect(), useState(), useMemo(), useId(), useCallback())
@@ -9,37 +9,53 @@ const Signup = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [age, setAge] = useState(16);
 
+  function increseAge(e){
+    e.preventDefault();
+    setAge(age + 1);
+  }
+  function decreaseAge(e){
+    e.preventDefault();
+    setAge(age - 1);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Signup clicked..!");
   }
-console.log(email);
+useEffect(() => {
+  console.log("age ", age);
+}, [age])
 
   return (
-    <>
-    <div>Signup</div>
-    <form>
-      <input 
-        className="input-box" 
-        type="email" 
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-
-      />
-      <input 
-        className="input-box" 
-        type="password" 
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)} 
-      
-      />
-      <button onClick={handleSubmit}>Submit</button>
-    </form>
-    </>
+          <>
+                <div>Signup</div>
+                <form>
+                  {age > 18 ? (
+                    <div>
+                        <input 
+                          className="input-box" 
+                          type="email" 
+                          placeholder="email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input 
+                          className="input-box" 
+                          type="password" 
+                          placeholder="password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)} 
+                        />
+                        <button onClick={handleSubmit}>Submit</button>
+                    </div>
+                   ) : (
+                      <h2>you are not 18, please increase your age..! </h2>
+                     ) }
+                  <button onClick={increseAge}>add age (+)</button> <button onClick={decreaseAge}>decrese age (-)</button>
+                </form>
+          </>
     
   )
 }
